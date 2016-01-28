4ba4d3f1d28c87e673cf4a659035755f50788c85
Daniel Gomez Rico <danielgomezrico@gmail.com>
2016-01-28 17:36:24 -0500

    First commit

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+CHANGELOG
+===============
+
+*V 0.0.1-SNAPSHOT*
++ Debug Drawer.
++ ViewServer.
---
+++ debugging/src/main/java/com/barista_v/debugging/ScalpeledActivity.java
@@ -0,0 +1,7 @@
+package com.barista_v.debugging;
+
+import com.jakewharton.scalpel.ScalpelFrameLayout;
+
+public interface ScalpeledActivity {
+  ScalpelFrameLayout getScalpelFrameLayout();
+}
