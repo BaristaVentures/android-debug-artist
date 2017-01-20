e51fd330ac04fffc7b8c319b18bb50d4ccc330ed
Daniel Gomez Rico <danielgomezrico@gmail.com>
2017-01-20 11:00:24 -0500

    Clean Retry after timeout management

--- lib/src/main/java/com/barista_v/debug_artist/utils/RetryAfterTimeoutWithDelay.kt
+++ lib/src/main/java/com/barista_v/debug_artist/utils/RetryAfterTimeoutWithDelay.kt
@@ -9,10 +9,13 @@ import java.util.concurrent.TimeUnit
 /**
  * Retry observable subscription if timeout.
  *
+ * For every retry it will wait delay + delayAmount so we wait more and more every retry.
+ *
  * @param maxRetries number of retries
- * @param initialDelay milliseconds of wait between each try
+ * @param delay milliseconds of wait between each try
+ * @param delayAmount  delay + delayAmount
  */
-class RetryAfterTimeoutWithDelay(val maxRetries: Int, var initialDelay: Long, val delay: Long = 100)
+class RetryAfterTimeoutWithDelay(val maxRetries: Int, var delay: Long, val delayAmount: Long = 100)
   : Func1<Observable<out Throwable>, Observable<*>> {
 
   internal var retryCount = 0
@@ -20,8 +23,8 @@ class RetryAfterTimeoutWithDelay(val maxRetries: Int, var initialDelay: Long, va
   override fun call(attempts: Observable<out Throwable>): Observable<*> {
     return attempts.flatMap({
       if (++retryCount < maxRetries && it is SocketTimeoutException) {
-        initialDelay += delay
-        Observable.timer(initialDelay, TimeUnit.MILLISECONDS)
+        delay += delayAmount
+        Observable.timer(delay, TimeUnit.MILLISECONDS)
       } else {
         Observable.error(it as Throwable)
       }
