8e5d27f79fd7f19319fc55c62449d89ccb19748e
Daniel Gomez Rico <danielgomezrico@gmail.com>
2017-05-22 14:07:13 -0500

    Update dependencies

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Install required dependencies
+# $ANDROID_HOME must be set to be cached on CI setup
+#
+
+DEPS_FILE="$ANDROID_HOME/android-dependencies-installed"
+
+if [ ! -e $DEPS_FILE ]; then
+
+  for I in android-25 \
+           build-tools-25.0.3  \
+           tool \
+           extra-android-m2repository \
+           extra-android-support \
+           extra-google-google_play_services \
+           extra-google-m2repository;
+   do echo y | android update sdk --no-ui --all --filter $I ; done
+
+   touch DEPS_FILE;
+
+ fi
